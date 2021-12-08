a827c56647d8f02e0fa99f11c00d1b73ea340a00
dmytro <dmytro.panchuk@globallogic.com>
2021-12-08 18:02:16 +0200

    Fixed console.log handler doesn't work on Firefox

--- tests/features/support/hooks.js
+++ tests/features/support/hooks.js
@@ -1,5 +1,6 @@
 import { Before, After, Status } from '@cucumber/cucumber'
 import wd from 'selenium-webdriver'
+import { browser } from '../../config'
 
 Before(async function() {
   await this.driver.manage().window()
@@ -10,21 +11,25 @@ After(async function(testCase) {
     var stream = await this.driver.takeScreenshot()
     await this.attach(stream, 'base64:image/png')
   }
-  const logs = await this.driver
-    .then(() =>
-      this.driver
-        .manage()
-        .logs()
-        .get(wd.logging.Type.BROWSER)
-    )
-    .then(result => {
-      return result
-    })
-  await this.driver.quit()
-  if (logs.some(log => log.level.name_ === 'SEVERE')) {
-    await logs.forEach(log =>
-      this.attach(`${log.level.name} ${log.message}`, 'text/plain')
-    )
-    throw new Error('There are some errors in console')
+  if (browser === 'chrome') {
+    const logs = await this.driver
+      .then(() =>
+        this.driver
+          .manage()
+          .logs()
+          .get(wd.logging.Type.BROWSER)
+      )
+      .then(result => {
+        return result
+      })
+    await this.driver.quit()
+    if (logs.some(log => log.level.name_ === 'SEVERE')) {
+      await logs.forEach(log =>
+        this.attach(`${log.level.name} ${log.message}`, 'text/plain')
+      )
+      throw new Error('There are some errors in console')
+    }
+  } else {
+    await this.driver.quit()
   }
 })
